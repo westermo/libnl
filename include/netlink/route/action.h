45f117b4f1363328efb2aa1e8579f43ebd1bff51
Magnus Öberg <magnus.oberg@westermo.se>
2016-04-27 11:31:20 +0200

    Added function rtnl_act_next() to return the next action object in the action list. If an object reference is returned, it must be "put" after use.
    Signed-off-by: Magnus Öberg <magnus.oberg@westermo.se>

--- include/netlink/route/action.h
+++ include/netlink/route/action.h
@@ -40,6 +40,7 @@ extern int		rtnl_act_delete(struct nl_sock *, struct rtnl_act *,
 					int);
 extern int		rtnl_act_append(struct rtnl_act **, struct rtnl_act *);
 extern int		rtnl_act_remove(struct rtnl_act **, struct rtnl_act *);
+extern struct rtnl_act *rtnl_act_next(struct rtnl_act *);
 extern int		rtnl_act_fill(struct nl_msg *, int, struct rtnl_act *);
 extern void		rtnl_act_put_all(struct rtnl_act **);
 extern int		rtnl_act_parse(struct rtnl_act **, struct nlattr *);
