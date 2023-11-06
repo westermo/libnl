4789c406a3d403c4d38df95d18ac6bb85e9820d9
Volodymyr Bendiuga <volodymyr.bendiuga@westermo.com>
2023-11-06 10:23:07 +0100

    linux: update headers
    Signed-off-by: Volodymyr Bendiuga <volodymyr.bendiuga@westermo.com>

--- include/linux-private/linux/if_link.h
+++ include/linux-private/linux/if_link.h
@@ -1127,6 +1127,7 @@ enum {
 	IFLA_HSR_PROTOCOL,		/* Indicate different protocol than
 					 * HSR. For example PRP.
 					 */
+	IFLA_HSR_EFT,		        /* Entry Forget Time */
 	__IFLA_HSR_MAX,
 };
 
