dbd7f55cd557b6d6a0f3c5c9c115f4f9d7f6de22
Volodymyr Bendiuga <volodymyr.bendiuga@westermo.se>
2025-08-21 16:36:00 +0200

    route: cls: flower: extend API
    The following API has been added:
    
        - rtnl_flower_set_src_port_range
        - rtnl_flower_get_src_port_range
        - rtnl_flower_set_dst_port_range
        - rtnl_flower_get_dst_port_range
        - rtnl_flower_set_tcp_src_port
        - rtnl_flower_set_tcp_dst_port
        - rtnl_flower_get_tcp_src_port
        - rtnl_flower_get_tcp_dst_port
        - rtnl_flower_set_udp_src_port
        - rtnl_flower_set_udp_dst_port
        - rtnl_flower_get_udp_src_port
        - rtnl_flower_get_udp_dst_port

--- include/netlink-private/types.h
+++ include/netlink-private/types.h
@@ -645,6 +645,14 @@ struct rtnl_flower
 	uint8_t          cf_ip_ttl_mask;
 	uint8_t          cf_icmpv6_type;
 	uint8_t          cf_icmpv6_type_mask;
+	uint16_t         cf_src_port_min;
+	uint16_t         cf_src_port_max;
+	uint16_t         cf_dst_port_min;
+	uint16_t         cf_dst_port_max;
+	uint16_t         cf_tcp_src_port;
+	uint16_t         cf_tcp_dst_port;
+	uint16_t         cf_udp_src_port;
+	uint16_t         cf_udp_dst_port;
 };
 
 struct rtnl_cgroup
